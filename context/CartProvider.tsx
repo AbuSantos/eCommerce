4eb66f759de5711b8fe56ff05d05966b7fcf1fa9
Abusantos <abusomwansantos@gmail.com>
2023-12-26 23:51:06 +0100

    updating the reducer function for the cartItems

--- context/CartProvider.tsx
+++ context/CartProvider.tsx
@@ -17,11 +17,31 @@ type CartStateType = { cart: CartItemType[] }
 const initCartState: CartStateType = { cart: [] }
 
 const REDUCER_ACTION_TYPE = {
-    ADD:"ADD",
-    REMOVE:"REMOVE",
-    
+    ADD: "ADD",
+    REMOVE: "REMOVE",
+    QUANTITY: "QUANTITY",
+    SUBMIT: "SUBMIT"
 }
 
+export type ReducerActionType = typeof REDUCER_ACTION_TYPE
+
+export type ReducerAction = {
+    type: string,
+    payload?: CartItemType
+}
+
+const reducer = (state: CartStateType, action: ReducerAction): CartStateType => {
+    switch (action.type) {
+        case REDUCER_ACTION_TYPE.ADD: {
+            
+        }
+            
+            break;
+    
+        default:
+            break;
+    }
+}
 const CartProvider = () => {
     return (
         <div>CartProvider</div>
